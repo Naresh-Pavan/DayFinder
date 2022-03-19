89ba4b0018144f97af4f6dad4f44b2c368d05d01
Naresh-Pavan <71119630+Naresh-Pavan@users.noreply.github.com>
2022-03-19 21:47:58 +0530

    completed

---
+++ script.js
@@ -0,0 +1,35 @@
+function dayofweek(d,m,y)
+{
+    let t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ];
+    y -= (m < 3) ? 1 : 0;
+    return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+}
+let d=parseInt(prompt("Enter your birth day"));
+let m=parseInt(prompt("enter your birth month"));
+let y=parseInt(prompt("enter your birth year"));
+let day = Math.round(dayofweek(d,m,y));
+let nameOfDay;
+switch(day){
+    case 0: 
+        nameOfDay = 'Sunday';  
+        break;
+    case 1:
+        nameOfDay = 'Monday';
+        break;
+    case 2:
+        nameOfDay = 'Tuesday';
+        break;
+    case 3:
+        nameOfDay = 'Wednesday';
+        break;
+    case 4:
+        nameOfDay = 'Thursday';
+        break;
+    case 5:
+        nameOfDay = 'Friday';
+        break;
+    case 6:
+        nameOfDay = 'Saturday';
+        break;
+}
+alert(nameOfDay);
